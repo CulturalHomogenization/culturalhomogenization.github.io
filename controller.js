9c83663a4c95833bf7f58c2a828d306b85fae896
CulturalHomogenization <rayyan04@kingshigh.school.nz>
2023-10-15 16:07:45 +1300

    HomePage Complete

--- controller.js
+++ controller.js
@@ -5,44 +5,18 @@ window.addEventListener('scroll', function(){
   header.classList.toggle('sticky', window.scrollY > 0);
 })
 
-//modals
+const indicator = document.querySelector('.indicator');
 
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-//
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal)
-//   })
-// })
-//
-// overlay.addEventListener('click', () => {
-//   const modals = document.querySelectorAll('.modal.active')
-//   modals.forEach(modal => {
-//     closeModal(modal)
-//   })
-// })
-//
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal')
-//     closeModal(modal)
-//   })
-// })
-//
-// function openModal(modal) {
-//   if (modal == null) return
-//   modal.classList.add('active')
-//   overlay.classList.add('active')
-// }
-//
-// function closeModal(modal) {
-//   if (modal == null) return
-//   modal.classList.remove('active')
-//   overlay.classList.remove('active')
-// }
+function handleScroll() {
+  const scrollPosition = window.scrollY;
+  const viewportHeight = window.innerHeight;
+  const fadeThreshold = viewportHeight / 2;
+
+  const opacity = 1 - (scrollPosition / fadeThreshold);
+
+  indicator.style.opacity = Math.max(opacity, 0);
+}
+window.addEventListener('scroll', handleScroll);
 
 //Menu controller
 function toggleMenu(){
